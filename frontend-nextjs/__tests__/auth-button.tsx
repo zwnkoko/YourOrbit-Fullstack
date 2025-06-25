dc8e40fa17e13b02909a050e2f260a0a5cca69a0
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2025-06-25 17:04:15 +0800

    test(frontend): create initial test file for AuthButton component

---
+++ frontend-nextjs/__tests__/auth-button.tsx
@@ -0,0 +1,4 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import { AuthButton } from "@/components/shared/auth-button";
+
+// ToDo: Add test for AuthButton after implementing it
